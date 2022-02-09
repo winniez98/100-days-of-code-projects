5fccb96346315880bfc2b31b114980a4d278a014
Winnie Zhang <75088108+winniez98@users.noreply.github.com>
2022-02-08 23:54:46 -0500

    Add files via upload

---
+++ Day17_QuizGame/main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from data import question_data
+
+# create question bank that contains list of question objects each initialized with question and answer
+question_bank = []
+# loop through question data
+for question in question_data:
+    # create Question object from each entry in question data
+    question_object = Question(question["text"], question["answer"])
+    # append each Question object to question bank
+    question_bank.append(question_object)
+
+# we're converting data which has str key (easy to make typos) and convert it object
+# -> easier and foolproof way to access
+
+print(question_bank[0].text)
+
+
+
