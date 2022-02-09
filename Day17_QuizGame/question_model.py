460093ad1469d8c2b958b6460d626d0a7fa296c1
Winnie Zhang <75088108+winniez98@users.noreply.github.com>
2022-02-08 23:41:19 -0500

    Add files via upload

---
+++ Day17_QuizGame/question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        # must use this syntax self.nameOfAttribute -> when create new object, will pass in these 2
+        self.text = q_text
+        self.answer = q_answer
